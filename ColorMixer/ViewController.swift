7ae80befed7060d3e4b0928abf9a20273e26ba28
Georgii Litvinenko <jyrki.ss@gmail.com>
2023-02-06 23:00:33 +0200

    v1

--- ColorMixer/ViewController.swift
+++ ColorMixer/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var colorView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        slidersSetup()
+        
+    }
+    
+//    @IBAction func blendColor() {
+//        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+//                                            green:CGFloat(greenSlider.value),
+//                                            blue: CGFloat(blueSlider.value),
+//                                            alpha: 1)
+ //   }
+//
+//  @IBAction func setLabelValue() {
+//       redLabel.text = String(format: "%.2f", redSlider.value)
+//      greenLabel.text = String(format: "%.2f", greenSlider.value)
+//      blueLabel.text = String(format: "%.2f", blueSlider.value)
+//
+//  }
+    
+    
+    private func slidersSetup () {
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.tintColor = .red
+        
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenSlider.tintColor = .green
+        
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueSlider.tintColor = .blue
     }
 
-
 }
+
 
