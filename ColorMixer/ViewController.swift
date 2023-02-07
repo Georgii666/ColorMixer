27bf013aa63e72ac42503958dcb54108c48e8cb5
Georgii Litvinenko <jyrki.ss@gmail.com>
2023-02-07 13:02:47 +0200

    hw2.03

--- ColorMixer/ViewController.swift
+++ ColorMixer/ViewController.swift
@@ -9,51 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var viewColor: UIView!
+    
     @IBOutlet var redLabel: UILabel!
     @IBOutlet var greenLabel: UILabel!
     @IBOutlet var blueLabel: UILabel!
+    
     
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    @IBOutlet var colorView: UIView!
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewColor.layer.cornerRadius = 8
         slidersSetup()
-        
     }
     
-//    @IBAction func blendColor() {
-//        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
-//                                            green:CGFloat(greenSlider.value),
-//                                            blue: CGFloat(blueSlider.value),
-//                                            alpha: 1)
- //   }
-//
-//  @IBAction func setLabelValue() {
-//       redLabel.text = String(format: "%.2f", redSlider.value)
-//      greenLabel.text = String(format: "%.2f", greenSlider.value)
-//      blueLabel.text = String(format: "%.2f", blueSlider.value)
-//
-//  }
+    @IBAction func blendColor() {
+        viewColor.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green:CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1)
+    }
     
-    
+    @IBAction func setLabelValue() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+ 
     private func slidersSetup () {
-        redSlider.minimumValue = 0
-        redSlider.maximumValue = 1
         redSlider.tintColor = .red
-        
-        greenSlider.minimumValue = 0
-        greenSlider.maximumValue = 1
         greenSlider.tintColor = .green
-        
-        blueSlider.minimumValue = 0
-        blueSlider.maximumValue = 1
         blueSlider.tintColor = .blue
     }
-
 }
 
 
